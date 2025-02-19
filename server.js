f103955098d2c39aba75aa5e0ba8ff8f2ca57e2f
Ananyanayaka <99317078+Ananyanayaka@users.noreply.github.com>
2025-02-19 16:56:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 
 const express = require('express');
 const axios = require('axios');
@@ -6,26 +5,21 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000; // Heroku will provide the correct port
+const port = process.env.PORT || 5000; 
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// NASA API key (store in .env)
 const NASA_API_KEY = process.env.NASA_API_KEY;
 
-// NASA API endpoints
 const NASA_APOD_URL = 'https://api.nasa.gov/planetary/apod';
 const NASA_EPIC_URL = 'https://api.nasa.gov/EPIC/api/natural';
 const NASA_MEDIA_URL = 'https://images-api.nasa.gov/search';
 
-// Route for root URL
 app.get('/', (req, res) => {
   res.send('Welcome to the NASA API Media Server!');
 });
 
-// Route for APOD (Astronomy Picture of the Day)
 app.get('/api/apod', async (req, res) => {
   try {
     const response = await axios.get(NASA_APOD_URL, {
@@ -66,7 +60,6 @@ app.get('/api/nasa-media', async (req, res) => {
   }
 });
 
-// Start server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
